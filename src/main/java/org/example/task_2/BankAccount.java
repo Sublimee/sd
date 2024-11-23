d86fd962d199dfc22588dec893f8ae3128a7207c
Gusev Maksim <magusev@alfabank.ru>
2024-11-23 08:02:40 +0300

    task_2

---
+++ src/main/java/org/example/task_2/BankAccount.java
@@ -0,0 +1,21 @@
+package org.example.task_2;
+
+public class BankAccount {
+    private double balance;
+
+    public BankAccount(double initialBalance) {
+        balance = initialBalance;
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    public void withdraw(double amount) {
+        balance -= amount;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+}
